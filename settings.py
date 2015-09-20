3914f2d73036ff2833aacc87c64a61c82f66aee7
Kyle Pittman <pittman.kyle@gmail.com>
2015-09-20 14:29:45 -0500

    Settings now are split between settings.py local_settings.py and secrets.py

--- settings.py
+++ settings.py
@@ -1,6 +1,5 @@
 import os
-from secrets import MYSQL_USER, MYSQL_PASS
+from local_settings import DATABASE_URI
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 APP_STATIC = os.path.join(APP_ROOT, 'static')
-DATABASE_URI = "mysql+pymysql://{user}:{password}@monkpit.mysql.pythonanywhere-services.com/monkpit$blog".format(user=MYSQL_USER, password=MYSQL_PASS)
 
