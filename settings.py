7ec16267940d5aeb6943d715c53850fa3caf2074
Kyle Pittman <pittman.kyle@gmail.com>
2015-09-20 18:54:44 +0000

    Fixed settings.py to get correct env vars, fixed accidental use of 'pass'

--- settings.py
+++ settings.py
@@ -1,8 +1,7 @@
 import os
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 APP_STATIC = os.path.join(APP_ROOT, 'static')
-MYSQL_USER = os.environ.get('blog_mysql_user', None)
-MYSQL_PASS = os.environ.get('blog_mysql_pass', None)
-DATABASE_URI = "mysql+pymysql://{user}:{pass}@monkpit.mysql.pythonanywhere-services.com/monkpit$mysite".format(
-        {'user': MYSQL_USER, 'pass': MYSQL_PASS})
+MYSQL_USER = os.environ.get('MYSQL_USER', None)
+MYSQL_PASS = os.environ.get('MYSQL_PASS', None)
+DATABASE_URI = "mysql+pymysql://{user}:{password}@monkpit.mysql.pythonanywhere-services.com/monkpit$mysite".format(user=MYSQL_USER, password=MYSQL_PASS)
 
