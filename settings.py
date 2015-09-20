c12ce82b1b026e1f4d492c90fe97b53349baf4b7
Kyle Pittman <pittman.kyle@gmail.com>
2015-09-20 19:17:52 +0000

    Environment variables won't work with pythonanywhere applications - break vars out into secrets.py

--- settings.py
+++ settings.py
@@ -1,7 +1,6 @@
 import os
+from secrets import MYSQL_USER, MYSQL_PASS
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 APP_STATIC = os.path.join(APP_ROOT, 'static')
-MYSQL_USER = os.environ.get('MYSQL_USER', None)
-MYSQL_PASS = os.environ.get('MYSQL_PASS', None)
-DATABASE_URI = "mysql+pymysql://{user}:{password}@monkpit.mysql.pythonanywhere-services.com/monkpit$mysite".format(user=MYSQL_USER, password=MYSQL_PASS)
+DATABASE_URI = "mysql+pymysql://{user}:{password}@monkpit.mysql.pythonanywhere-services.com/monkpit$blog".format(user=MYSQL_USER, password=MYSQL_PASS)
 
