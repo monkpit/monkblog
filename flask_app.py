544f9a0daf20fb0b07376e72ff65227a687f07d3
PittmanK2 <kyle.pittman@nov.com>
2015-09-22 16:21:27 -0500

    Load blueprints automatically based upon parent package - flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,16 +1,17 @@
 #! /usr/bin/env python
 
 from monkblog.app import app
+from flask import Blueprint
+import importlib, pkgutil
 
 def register_blueprints(flask_app, parent_modules):
-    from flask import Blueprint
     for parent_module in parent_modules:
-        imported_module = __import__(parent_module, globals(), locals(), ['object'], -1)
-        blueprints = [cls for name, cls
-                        in imported_module.__dict__.items()
-                        if isinstance(cls, Blueprint)]
-        for blueprint in blueprints:
-            flask_app.register_blueprint(blueprint)
+        loaded_module = importlib.import_module(parent_module)
+        blueprint_names = [name for file, name, ispkg in pkgutil.iter_modules(loaded_module.__path__) if ispkg is False]
+        for blueprint_name in blueprint_names:
+            blueprint_module = importlib.import_module(parent_module + '.' + blueprint_name)
+            for cls in [cls for name, cls in blueprint_module.__dict__.items() if isinstance(cls, Blueprint)]:
+                flask_app.register_blueprint(cls)
 
 register_blueprints(app, ['monkblog.blueprints'])
 
