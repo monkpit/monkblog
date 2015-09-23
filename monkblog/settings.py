1a25891d11915d7fe3abc1390dbc7cde4b2ae8ce
PittmanK2 <kyle.pittman@nov.com>
2015-09-23 08:51:49 -0500

    Added footer

--- monkblog/settings.py
+++ monkblog/settings.py
@@ -2,4 +2,4 @@ import os
 from local_settings import DATABASE_URI
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 APP_STATIC = os.path.join(APP_ROOT, 'static')
-
+CONNECTION_POOL_RECYCLE = 4
