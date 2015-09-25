06ef9adbd3a2721e873fc14831fba6c68b10c7a7
PittmanK2 <kyle.pittman@nov.com>
2015-09-25 10:18:48 -0500

    Working sitemap.xml, needs static files added.

--- monkblog/blueprints/site_blueprint.py
+++ monkblog/blueprints/site_blueprint.py
@@ -1,9 +1,11 @@
 from flask import Flask, send_from_directory, request, Blueprint, \
-                    render_template, url_for, redirect, abort
+                    render_template, url_for, redirect, abort, make_response
 import os
+from datetime import datetime, timedelta
 
-from monkblog.models import Post, Category
+from monkblog.models import Post, Author
 from monkblog.database import db
+from monkblog.app import app
 
 site_blueprint = Blueprint('site_blueprint', __name__, template_folder='templates')
 
@@ -12,6 +14,35 @@ def post_index():
     post_objects = db.session.query(Post).filter_by(passphrase=None).order_by(Post.pub_date.desc()).limit(10).all()
     bootstrap_theme = request.args.get('theme', 'spacelab')
     return render_template('mysite/post_index.html', posts=post_objects, bootstrap_theme=bootstrap_theme)
+
+@site_blueprint.route('/sitemap.xml')
+def sitemap():
+    pages=[]
+    ten_days_ago=(datetime.now() - timedelta(days=10)).date().isoformat()
+    # static pages
+    for rule in app.url_map.iter_rules():
+        if "GET" in rule.methods and len(rule.arguments)==0:
+            pages.append(
+                       [rule.rule,ten_days_ago]
+                       )
+
+    # author model pages
+    authors = db.session.query(Author).order_by(Author.slug).all()
+    for author in authors:
+        url=url_for('model_blueprint.show_author',slug=author.slug)
+        pages.append([url])
+
+    # post model pages
+    posts = db.session.query(Post).order_by(Post.pub_date).all()
+    for post in posts:
+        url=url_for('model_blueprint.post_from_db',slug=post.slug)
+        pages.append([url])
+
+    sitemap_xml = render_template('base/sitemap.xml', pages=pages)
+    response= make_response(sitemap_xml)
+    response.headers["Content-Type"] = "application/xml"
+
+    return response
 
 @site_blueprint.errorhandler(404)
 def page_not_found(e):
